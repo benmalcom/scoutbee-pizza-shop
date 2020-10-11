03bbc09de1bb63884c64aa4185ee18ee462a3cae
Benjamin Ikeji <bikeji@ad1.systemadmin.com>
2020-10-11 12:00:46 +0100

    Reactor

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ It correctly bundles React in production mode and optimizes the build for the be
 
 ### `Follow up answers`
 
+1. 
--- src/data/mock/index.ts
+++ src/data/mock/index.ts
@@ -1,5 +1,4 @@
 import { v4 as uuid } from 'uuid';
-import { sample } from 'lodash';
 
 export interface PizzaIngredient {
   name: string;
@@ -14,12 +13,6 @@ export interface Pizza {
   timestamp: number
 }
 
-const pizzaNames: string[] = [
-  'Margherita', 'Marinara', 'Quattro Stagioni', 'Carbonara', 'Frutti di Mare',
-  'Romana', 'Fattoria', 'Schiacciata', 'Prosciutto', 'Quattro Formaggi', 'Crudo',
-  'Napoletana', 'Pugliese', 'Montanara', 'Emiliana', 'Americana', 'Sarda', 'Tonno',
-];
-
 export const pizzaStatuses: string[] = ['Waiting', 'Ready'];
 
 export const pizzaIngredients: PizzaIngredient[] = [
@@ -27,28 +20,5 @@ export const pizzaIngredients: PizzaIngredient[] = [
   { name: 'Bacon', id: uuid() },
   { name: 'Mushrooms', id: uuid() },
   { name: 'Ananas', id: uuid() },
-  { name: 'Tomato sauce', id: uuid() },
-  { name: 'Mozzarella', id: uuid() },
-  { name: 'Parmesan', id: uuid() },
-  { name: 'Eggs', id: uuid() },
-  { name: 'Bacon', id: uuid() },
 ];
-
-export const createMockPizzaList = () => {
-  // Generate 30 mock pizza
-  const items: Pizza[] = [];
-  for (let i = 0; i < 30; i++) {
-    const name: string = sample(pizzaNames) || pizzaNames[0];
-    const status: string = sample(pizzaStatuses) || pizzaStatuses[0];
-    const randomSize =  Math.floor(Math.random() * (pizzaIngredients.length - 1 + 1)) + 1;
-    items.push({
-      id: uuid(),
-      name,
-      status,
-      ingredients: pizzaIngredients.slice(0, randomSize),
-      timestamp: Date.now()
-    });
-  }
-  return items;
-};
 
